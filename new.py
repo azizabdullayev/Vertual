ca172c7dc35b02fd2141cf6d300b33553e29397f
User <azizbekabdullayev2828@gmail.com>
2023-03-18 18:52:33 +0500

    first commit

---
+++ new.py
@@ -0,0 +1,16 @@
+# remove duplicates
+# a = ["a", "b", "c", "a", "d", "c"]
+# b = list(dict.fromkeys(a))
+# print(b)
+
+
+# a = ["a", "b", "c", "a", "d", "c"]
+# print(list(set(a)))
+
+
+# a = ["a", "b", "c", "a", "d", "c"]
+# b = []
+# for i in a:
+#   if i not in b:
+#     b.append(i)
+# print(b)
